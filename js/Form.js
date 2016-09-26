f5dc037fc2da4171d13fa467890e5f94f259b872
Casper Björk <erik.casper.bjork@gmail.com>
2016-09-26 18:54:02 +0200

    Starting to work on form, will however delete
    Doing some work with JS but after talking to the owner will that be
    changed.

--- js/Form.js
+++ js/Form.js
@@ -1,5 +1,14 @@
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+var shortside;
+var longside;
+var height;
+
+$('#kostnadsform').submit(function () {
+ collectValues();
+ return false;
+});
+
+function collectValues(){
+    longside = document.getElementById("långinput").value;
+    shortside = document.getElementById("kortinput").value;
+    height = document.getElementById("höjdinput").value;
+}
